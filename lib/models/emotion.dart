071f8f95dde8f05a84549cae8ad699a35a6b6884
Radiant-code <Rayaan.rajabally@gmail.com>
2025-06-22 14:36:55 +1200

    Add Emotion model and mock emotion data

---
+++ lib/models/emotion.dart
@@ -0,0 +1,15 @@
+class Emotion {
+  final String name;
+  final String sector;
+  final int intensity;
+  final List<String> tags;
+  final String description;
+
+  Emotion({
+    required this.name,
+    required this.sector,
+    required this.intensity,
+    required this.tags,
+    required this.description,
+  });
+}
